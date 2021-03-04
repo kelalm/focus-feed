b3b4d7e3f59175177588c377aac9c1373c98db6d
kelalm <kelvinalmonte@live.com>
2021-03-03 22:38:35 -0500

    modified home page text and altered jest tests

--- __tests__/HomeTest.js
+++ __tests__/HomeTest.js
@@ -4,12 +4,16 @@ import Home from "../pages/index";
 
 test("Check for Getting Started Text", () => {
   const { getByText } = render(<Home />);
-  expect(getByText("Get started by editing")).toBeInTheDocument();
+  expect(
+    getByText(
+      "Focus Feed is a public pomodoro feed to motivate you to stay focused and track your progress over time."
+    )
+  ).toBeInTheDocument();
 });
 
 it("Renders appropriately", () => {
   render(<Home />);
   expect(
-    screen.getByRole("heading", { name: "Welcome to Next.js!" })
+    screen.getByRole("heading", { name: "Welcome to Focus Feed" })
   ).toBeInTheDocument();
 });
